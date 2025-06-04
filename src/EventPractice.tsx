748e6aac95f4112b2e82fdd8165ea301da81a884
akh820 <akh8202@gmail.com>
2025-06-04 16:11:53 +0900

    feat: 4.3 함수 컴포넌트 객체 복사 및 객체 덮어쓰기 규칙

--- src/EventPractice.tsx
+++ src/EventPractice.tsx
@@ -2,27 +2,37 @@ import React, { useState } from "react";
 
 const EventPractice = () => {
 
-    const [username, setUsername] = useState("");
-    const [message, setMessage] = useState("");
-    //타입스크립트는 들어오는 이벤트도 표시해줘야 한다.
-    const onChangeUsername = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setUsername(e.target.value);
-    }
-    const onChangeMessage = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setMessage(e.target.value);
-    }
-    const onClick = () => {
-        alert(username + ": " + message);
-        setUsername("");
-        setMessage("");
+    const [form, setForm] = useState({
+        username: "",
+        message: "",
+        password: ""
+    });
+
+    const { username, message, password } = form;
+
+    //js 객체 속성 덮어쓰기 규칙때문에 나중에 온 값으로 덮어 씌어진다.
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const nextForm = {
+            ...form,
+            [e.target.name]: e.target.value
+        };
+        setForm(nextForm);
     }
 
-    const onKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    const onClick = () => {
+        alert(username + " : " + message + " : " + password);
+        setForm({
+            username: "",
+            message: "",
+            password: ""
+        });
+    }
+
+    const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
         if (e.key === "Enter") {
             onClick();
         }
     }
-
     return (
         <div>
             <h1>이벤트 연습</h1>
@@ -31,15 +41,24 @@ const EventPractice = () => {
                 name="username"
                 placeholder="사용자명"
                 value={username}
-                onChange={onChangeUsername}
+                onChange={onChange}
+                onKeyDown={onKeyDown}
             />
             <input
                 type="text"
                 name="message"
                 placeholder="아무거나 입력해 보세요"
                 value={message}
-                onChange={onChangeMessage}
-                onKeyPress={onKeyPress}
+                onChange={onChange}
+                onKeyDown={onKeyDown}
+            />
+            <input
+                type="text"
+                name="password"
+                placeholder="비밀번호"
+                value={password}
+                onChange={onChange}
+                onKeyDown={onKeyDown}
             />
             <button onClick={onClick}>확인</button>
         </div>
